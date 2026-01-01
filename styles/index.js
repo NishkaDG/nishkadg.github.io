3bab5b871ce673e807ed28acdaa553c959e97e23
Nishka Dasgupta <=>
2026-01-01 19:43:30 +0100

    Remove extra code

--- styles/index.js
+++ styles/index.js
@@ -6,65 +6,26 @@ var hide_lists = function(cb) {
 	$('#papers-btn').removeClass('disabled');
     $('#stories-btn').removeClass('disabled');
 };
-var show_projects = function() {
-	$('#cv-btn').removeClass('disabled');
-	$('#cv').fadeOut(300);
-	$('#papers-btn').removeClass('disabled');
-	$('#papers').fadeOut(300);
-    $('#stories-btn').removeClass('disabled');
-	$('#stories').fadeOut(300);
-    $('#posts-btn').removeClass('disabled');
-    $('#posts').fadeOut(300, function() {
-        $('#projects').fadeIn(300)
-    });
-    $('#projects-btn').addClass('disabled')
-};
 var show_posts = function() {
-	$('#cv-btn').removeClass('disabled');
-	$('#cv').fadeOut(300);
 	$('#papers-btn').removeClass('disabled');
 	$('#papers').fadeOut(300);
     $('#stories-btn').removeClass('disabled');
 	$('#stories').fadeOut(300);
-    $('#projects-btn').removeClass('disabled');
-    $('#projects').fadeOut(300, function() {
-        $('#posts').fadeIn(300)
-    });
+    $('#posts').fadeIn(300)
     $('#posts-btn').addClass('disabled')
 };
-var show_cv = function() {
-    $('#posts-btn').removeClass('disabled');
-    $('#posts').fadeOut(300);
-	$('#papers-btn').removeClass('disabled');
-	$('#papers').fadeOut(300);
-    $('#stories-btn').removeClass('disabled');
-	$('#stories').fadeOut(300);
-	$('#projects-btn').removeClass('disabled');
-    $('#projects').fadeOut(300, function() {
-        $('#cv').fadeIn(300)
-    });
-    $('#cv-btn').addClass('disabled')
-};
+
 var show_papers = function() {
     $('#posts-btn').removeClass('disabled');
     $('#posts').fadeOut(300);
-	$('#cv-btn').removeClass('disabled');
-	$('#cv').fadeOut(300);
     $('#stories-btn').removeClass('disabled');
 	$('#stories').fadeOut(300);
-	$('#projects-btn').removeClass('disabled');
-    $('#projects').fadeOut(300, function() {
-        $('#papers').fadeIn(300)
-    });
+    $('#papers').fadeIn(300);
     $('#papers-btn').addClass('disabled')
 };
 var show_stories = function() {
     $('#posts-btn').removeClass('disabled');
     $('#posts').fadeOut(300);
-	$('#cv-btn').removeClass('disabled');
-	$('#cv').fadeOut(300);
-	$('#projects-btn').removeClass('disabled');
-    $('#projects').fadeOut(300);
     $('#papers-btn').removeClass('disabled');
     $('#papers').fadeOut(300, function() {
         $('#stories').fadeIn(300)
